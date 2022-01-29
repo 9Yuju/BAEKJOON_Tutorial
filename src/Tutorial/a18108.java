2cd76af95a448f5b44503497608f9f33a35cc043
9Yuju <qkrdbwn98@naver.com>
2022-01-29 16:44:19 +0900

    220129

--- src/Tutorial/a18108.java
+++ src/Tutorial/a18108.java
@@ -4,16 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a8393 {
-
+public class a18108 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(bf.readLine());
-		int sum=0;
-		for (int i = 1; i < n+1; i++) {
-			sum += i;
-		}
-		System.out.println(sum);
+		System.out.println(Integer.parseInt(bf.readLine())-543);
 	}
-
 }
