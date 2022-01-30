21d2553f7d96d1120637082ee3ac765997d050af
9Yuju <qkrdbwn98@naver.com>
2022-01-30 14:58:46 +0900

    220130

--- src/Tutorial/a1110.java
+++ src/Tutorial/a1110.java
@@ -8,29 +8,37 @@ public class a1110 {
 	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(bf.readLine());
-
-		int a = 0, b = 0;
-		a = n / 10;
-		b = n % 10;
-
+		
+		int T = n;
 		int count = 0;
-		int x = 0, k = 0;
-		if(n==0) {
-			System.out.println(1);
-		}else {
-		while (!(k == n)) {
-			x = a + b;
-			if (x >= 10)
-				x = x % 10;
-			k = b * 10 + x;
-
-			a = k / 10;
-			b = k % 10;
+		do {
+			n = ((n%10)*10)+(((n/10)+(n%10))%10);
 			count++;
-
-		}
-		// 출력
+		} while (!(T==n));
 		System.out.println(count);
-		}
+		
+//		int a = 0, b = 0;
+//		a = n / 10;
+//		b = n % 10;
+//
+//		int count = 0;
+//		int x = 0, k = 0;
+//		if(n==0) {
+//			System.out.println(1);
+//		}else {
+//		while (!(k == n)) {
+//			x = a + b;
+//			if (x >= 10)
+//				x = x % 10;
+//			k = b * 10 + x;
+//
+//			a = k / 10;
+//			b = k % 10;
+//			count++;
+//
+//		}
+//		// 출력
+//		System.out.println(count);
+//		}
 	}
 }
