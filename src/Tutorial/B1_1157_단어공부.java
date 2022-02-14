e9bee4e2b9074384dc0eb53652f8ac12af28a9d9
9Yuju <qkrdbwn98@naver.com>
2022-02-15 00:59:38 +0900

    220215

--- src/Tutorial/B1_1157_단어공부.java
+++ src/Tutorial/B1_1157_단어공부.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a1157 {
+public class B1_1157_단어공부 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
