392420e2c34dcdf71c9c4e88db79aa2da306ee8e
9Yuju <qkrdbwn98@naver.com>
2022-02-15 00:40:17 +0900

    220215

--- src/Tutorial/B1_1110_더하기사이클.java
+++ src/Tutorial/B1_1110_더하기사이클.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a1110 {
+public class B1_1110_더하기사이클 {
 	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(bf.readLine());
