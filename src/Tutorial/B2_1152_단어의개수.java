17b9ebbf2e9f86e7dd4511f39f8343f9905b9da7
9Yuju <qkrdbwn98@naver.com>
2022-02-16 01:41:54 +0900

    220216

--- src/Tutorial/B2_1152_단어의개수.java
+++ src/Tutorial/B2_1152_단어의개수.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a1152 {
+public class B2_1152_단어의개수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
