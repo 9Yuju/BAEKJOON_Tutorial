dbe96d7f8b9fcc6d317c9ab9313490bd5087f84d
9Yuju <qkrdbwn98@naver.com>
2022-02-18 00:13:57 +0900

    220218

--- src/Tutorial/B2_2577_숫자의개수.java
+++ src/Tutorial/B2_2577_숫자의개수.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a2577 {
+public class B2_2577_숫자의개수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(bf.readLine());
