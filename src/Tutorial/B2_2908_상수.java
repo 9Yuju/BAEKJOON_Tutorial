c54925d3bd388f9143cdcdf7175274fda21ccdac
9Yuju <qkrdbwn98@naver.com>
2022-02-19 20:57:21 +0900

    220219

--- src/Tutorial/B2_2908_상수.java
+++ src/Tutorial/B2_2908_상수.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class a2908 {
+public class B2_2908_상수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
