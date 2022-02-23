f0b551fef73935be80356d2d2d97181cb2dc54cc
9Yuju <qkrdbwn98@naver.com>
2022-02-24 00:41:54 +0900

    220224

--- src/Tutorial/B3_11022_A더하기B8.java
+++ src/Tutorial/B3_11022_A더하기B8.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class a11022 {
+public class B3_11022_A더하기B8 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
