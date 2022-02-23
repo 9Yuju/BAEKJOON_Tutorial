5ca3a6e3d3b13ef8e26733a490b3ec64aaa45248
9Yuju <qkrdbwn98@naver.com>
2022-02-24 00:41:50 +0900

    220224

--- src/Tutorial/B3_10950_A더하기B3.java
+++ src/Tutorial/B3_10950_A더하기B3.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 //for A+B-3
-public class a10950 {
+public class B3_10950_A더하기B3 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(bf.readLine());
