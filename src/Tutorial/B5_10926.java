c15c7be4e50348beb9f168904a8af4d3e6d56be3
9Yuju <qkrdbwn98@naver.com>
2022-02-24 00:42:00 +0900

    220224

--- src/Tutorial/B5_10926.java
+++ src/Tutorial/B5_10926.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a10926 {
+public class B5_10926 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
