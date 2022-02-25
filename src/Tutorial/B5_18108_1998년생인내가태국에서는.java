d34d2eaf2d134bd00ad71f29244b324061dd6b4b
9Yuju <qkrdbwn98@naver.com>
2022-02-25 15:26:02 +0900

    220225

--- src/Tutorial/B5_18108_1998년생인내가태국에서는.java
+++ src/Tutorial/B5_18108_1998년생인내가태국에서는.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a18108 {
+public class B5_18108_1998년생인내가태국에서는 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println(Integer.parseInt(bf.readLine())-543);
