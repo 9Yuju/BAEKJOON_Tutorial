a171db80f632362fc1db7a0143e05c1391df4a34
9Yuju <qkrdbwn98@naver.com>
2022-02-25 15:26:12 +0900

    220225

--- src/Tutorial/S4_1065_한수.java
+++ src/Tutorial/S4_1065_한수.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class a1065 {
+public class S4_1065_한수 { // 한수
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());// 123
